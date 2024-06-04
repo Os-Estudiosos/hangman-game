d1980ddf4ad83cf27f3f43b6ed53b763362995cf
doisgameplay <mat.vsp@gmail.com>
2024-06-04 18:56:17 -0300

    agora é possível trabalhar com palavras com acentos

--- game.py
+++ game.py
@@ -1,43 +1,72 @@
 import platform
-import os
+#import os
+import unicodedata
+
+#pede uma palavra
+
+#temp = get_gpt_word(letters, idiome, difficulty)
 
 temp = input("Escolha uma palavra\n")
 
+
+#Retira os acentos da palavra e cria uma lista temporária
+texto_normalizado = unicodedata.normalize('NFD', temp)
+temp_s = ''.join(c for c in texto_normalizado if unicodedata.category(c) != 'Mn')
+
+#cria a lista com os caracteres das palavra original
 palavra = []
 for char in temp:
     palavra.append(char)
 
-pinto = []
 
+#cria uma lista com os caracteres da palavra sem acento
+palavra_sem_acento = []
+for char in temp_s:
+    palavra_sem_acento.append(char)
+
+
+#cria a lista que será mostrada para o usuário
+lista = []
 for char in palavra:
-    pinto.append('_')
+    lista.append('_')
+
+#mostra a lista na tela
+for letter in lista:
+    print(letter, end = ".")
     
-for cu in pinto:
-    print(cu, end = ".")
-    
-cupinto = len(pinto)
+
+
+
 vidas = 6
-os.system("cls")
-while(pinto != palavra and vidas != 0):    
+#os.system("cls")
+while(lista != palavra and vidas != 0):    
+    
+    #Lê uma letra ou a palavra do usuário
     letra = input("\ndiga uma letra:")
     if(letra == temp):
-        for cu in palavra:
-            print(cu, end ="")
+        for letter in palavra:
+            print(letter, end ="")
         break    
     lost = "yes"
-    for index,char in enumerate(palavra):
+    cac = unicodedata.normalize('NFD', letra)
+    letra = ''.join(c for c in cac if unicodedata.category(c) != 'Mn')
+    for index,char in enumerate(palavra_sem_acento):
         if(letra == char):
-            pinto[index] = letra
+            #caso a letra exista na palavra, ela aparece no usuário 
+            lista[index] = palavra[index]
             lost = "no"
     if(lost == "yes"):
         vidas -= 1
-    os.system("cls")
-    for cu in pinto:
-        print(cu, end = ".")
+#os.system("cls")
+    for letter in lista:
+        print(letter, end = ".")
     print("    vidas: ", vidas)
 if(vidas == 0):
     print('\nPerdeu')
 else:
     print("\nPabens")
+    
+    
+
    
 
