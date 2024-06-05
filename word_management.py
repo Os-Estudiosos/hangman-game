8dfda9c1ca560715869e0ff2e8288c5b7dfbcb00
jaopredo <joao.profissional.45@gmail.com>
2024-06-05 18:33:54 -0300

    Chave api parte das configurações

--- word_management.py
+++ word_management.py
@@ -5,10 +5,9 @@ from dotenv import load_dotenv
 import os
 
 # Função que pega as letras, o idioma e a dificuldade e retorna uma palavra e uma dica
-def get_gpt_word(theme, language, difficulty):
+def get_gpt_word(theme, language, difficulty, api_key):
 
     # Puxo os dados do Chat GPT, aplicando as variáveis que estão no arquivo .json (letras, idioma, dificuldade)
-    api_key = os.getenv("OPENAI_API_KEY")
     client = OpenAI(api_key=api_key)  # Usando a chave do arquivo de configuração
     prompt = [{"role": "user", "content": f"Escolha uma palavra aleatória do dicionário, sem hifen e espaco, com o tema {theme}, no idioma {language}, de nível {difficulty}, dê um a dica sem usar a palavra, mande a dica na lingua {language}, e mande como uma string JSON"}]
     response = client.chat.completions.create(messages=prompt, model="gpt-3.5-turbo", max_tokens=100, temperature=1)
