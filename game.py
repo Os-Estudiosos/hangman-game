af6ab8fb5349cb4521e3ed76bf3e8f98bf6fa73e
Bruno Ferreira <brunoferreira17k@gmail.com>
2024-06-05 18:09:01 -0300

    Vinculação do backend e frontend

--- game.py
+++ game.py
@@ -1,10 +1,12 @@
 import unicodedata 
 
+# Retiro os acentos da palavara
 def tira_acento(palavra):
     texto_normalizado = unicodedata.normalize('NFD', palavra)
     temp_s = ''.join(c for c in texto_normalizado if unicodedata.category(c) != 'Mn')
     return (temp_s)
 
+# Checo se string/char está dentro da palavra e retorno uma lista com as posições (se for True) ou uma lista vazia (se for False) ou acertou se for a palavra
 def check_word(palavra, content):
     
     temp_s = tira_acento(palavra).lower()
@@ -15,19 +17,24 @@ def check_word(palavra, content):
     for char in temp_s:
         palavra_sem_acentos.append(char)
     
+    # Se for exatamente a palavra, retorno venceu e uma lista vazia
     if(content == temp_s):
         return{"variavel" : "venceu", "index_s" : [] }
+
     variavel = "false"
     
     index_s = []
         
     index = 0
     
+    # Faço uma lista com as posições em que a letra está
     for char in temp_s:
         if(char == content):
             index_s.append(index)
             variavel = "true"
         index += 1
-    return {"variavel" : variavel, "index_s" : index_s}           
+    
+    # Retorno uma lista com True (posições que a letra está) e False 
+    return {"variavel": variavel,"index_s": index_s}           
 
 
--- word_management.py
+++ word_management.py
@@ -1,37 +1,27 @@
 import json
+from game import check_word
 from openai import OpenAI
 from dotenv import load_dotenv
 import os
 
-# Função para ler o arquivo "config.json"
-def read_config():
-    with open("config.json", "r", encoding="utf-8") as file:
-        file_json = json.load(file)
-    return file_json
-
 # Função que pega as letras, o idioma e a dificuldade e retorna uma palavra e uma dica
 def get_gpt_word(letters, language, difficulty):
-    read_config()  # Leio o arquivo
 
     # Puxo os dados do Chat GPT, aplicando as variáveis que estão no arquivo .json (letras, idioma, dificuldade)
     api_key = os.getenv("OPENAI_API_KEY")
     client = OpenAI(api_key=api_key)  # Usando a chave do arquivo de configuração
     prompt = [{"role": "user", "content": f"Dê-me uma palavra de {letters} letras em {language} de nível {difficulty} para jogar o jogo da forca e dê uma dica sem usar a palavra"}]
-    response = client.chat.completions.create(messages=prompt, model="gpt-3.5-turbo", max_tokens=50, temperature=0)
+    response = client.chat.completions.create(messages=prompt, model="gpt-3.5-turbo", max_tokens=100, temperature=1)
 
     # Pego a resposta do Chat
     response_text = response.choices[0].message.content
 
     # Separo a palavra e a dica
-    try:
-        word = response_text.split('Palavra: ')[1].split('\n')[0].strip()
-        hint = response_text.split('Dica: ')[1].strip()
-    except IndexError:
-        word = ""
-        hint = response_text.strip()
+    word = response_text.split('Palavra: ')[1].split('\n')[0].strip()
+    hint = response_text.split('Dica: ')[1].strip()
 
-
+    
     # Retorno a palavra e a dica
-    return {"word" : word, "hint" : hint}
+    return {"word": word, "hint": hint}
 
-print(get_gpt_word(10, "português brasileiro", "difícil"))
+
